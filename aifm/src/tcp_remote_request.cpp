ac89fef4391c447f683568c18eccc79b3715cc2c
ZSK <ZSK@node-0.daifm.daifm-pg0.utah.cloudlab.us>
2022-07-07 05:50:30 -0600

    start device request programming

--- aifm/src/tcp_remote_request.cpp
+++ aifm/src/tcp_remote_request.cpp
@@ -1,5 +1,31 @@
+extern "C" {
+#include <runtime/runtime.h>
+}
+
+#include "array.hpp"
+#include "device.hpp"
+#include "helpers.hpp"
+#include "manager.hpp"
+
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <string>
+
+using namespace far_memory;
+using namespace std;
+
+constexpr static uint64_t kCacheSize = (128ULL << 20);
+constexpr static uint64_t kFarMemSize = (4ULL << 30);
+constexpr static uint32_t kNumGCThreads = 12;
+constexpr static uint32_t kNumEntries =
+    (8ULL << 20); // So the array size is larger than the local cache size.
+constexpr static uint32_t kNumConnections = 300;
+
 /*
-*get:obj_id, ds_id, obj_id_len, device_id
+// get:obj_id, ds_id, obj_id_len, device_id
 
 //首先根据device_id确认数据在哪里，然后根据device_index选择ip绑定连接
 //参考test_tcp文件 main()function
@@ -19,3 +45,52 @@
     （1）如果数据在caller的local_node，要用address把数据拿到，直接把数据取出来，通过tcp传回去；
     （2）如果数据本身在caller的远端，就通过object_id来获取；
 */
+// void TCPDevice::read_object(uint8_t ds_id, uint8_t obj_id_len,
+//                             const uint8_t *obj_id, uint16_t *data_len,
+//                             uint8_t *data_buf)
+void do_work(FarMemDevice *device){
+  device->read_object()
+
+}
+
+void run(netaddr raddr) {
+  BUG_ON(madvise(all_gen_reqs, sizeof(Req) * kNumReqs, MADV_HUGEPAGE) != 0);
+  FarMemDevice* device = new TCPDevice(raddr, kNumConnections, kFarMemSize);
+  do_work(device);
+}
+
+int argc;
+void _main(void *arg) {
+  char **argv = static_cast<char **>(arg);
+  std::string ip_addr_port1(argv[1]);//ip_addr_port1 is the node_0(compute node)
+  auto raddr1 = helpers::str_to_netaddr(ip_addr_port1);
+  std::string ip_addr_port2(argv[2]);//ip_addr_port2 is the node_2(another memory node)
+  auto raddr2 = helpers::str_to_netaddr(ip_addr_port2);
+  // run(raddr1);
+  run(raddr2);
+
+}
+
+int main(int _argc, char *argv[]) {
+  int ret;
+
+  if (_argc < 3) {
+    std::cerr << "usage: [cfg_file] [ip_addr:port]..." << std::endl;
+    return -EINVAL;
+  }
+
+  char conf_path[strlen(argv[1]) + 1];
+  strcpy(conf_path, argv[1]);
+  for (int i = 2; i < _argc; i++) {
+    argv[i - 1] = argv[i];
+  }
+  argc = _argc - 1;
+
+  ret = runtime_init(conf_path, _main, argv);
+  if (ret) {
+    std::cerr << "failed to start runtime" << std::endl;
+    return ret;
+  }
+
+  return 0;
+}
